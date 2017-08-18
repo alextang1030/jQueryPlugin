8f1f69813e88e69e53b0038f243f9ffa00b307ce
alex.tang <alex.tang@vicosys.com.hk>
2017-08-19 03:33:18 +0800

    Add after close function option Add close function call

--- lightbox/vclightbox.js
+++ lightbox/vclightbox.js
@@ -10,88 +10,104 @@
 			youtubeUrl : "",
 			width : null,
 			height: null,
+			afteClose: function(){},
 		},options,{
 			id: "vclb"+$.now(),
 			ele	:	null,
-			isIE : /*@cc_on!@*/false || !!document.documentMode,
-			AnimationEvent: function(){
-			  var t,
-			      el = document.createElement("fakeelement");
-
-			  var animations = {
-			    "animation"      : "animationend",
-			    "OAnimation"     : "oAnimationEnd",
-			    "MozAnimation"   : "animationend",
-			    "WebkitAnimation": "webkitAnimationEnd"
-			  }
-
-			  for (t in animations){
-			    if (el.style[t] !== undefined){
-			      return animations[t];
-			    }
-			  }
-			},
-			getId : function(url) {
-			    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-			    var match = url.match(regExp);
-
-			    if (match && match[2].length == 11) {
-			        return match[2];
-			    } else {
-			        return 'error';
-			    }
-			}
+			isIE : /*@cc_on!@*/false || !!document.documentMode
 		});
-		if (setting.youtubeEmbed && setting.youtubeUrl != "")
-		{
-			setting.content += '<iframe class="vc-lb-youtube" width="'+(setting.width == null ? '640px' : setting.width) +'" '+
-				'height="'+(setting.height == null ? '360px' : setting.height) +'" '+
-				'src="https://www.youtube.com/embed/'+setting.getId(setting.youtubeUrl)+'" frameborder="0" allowfullscreen></iframe>'
+		var AnimationEvent = function(){
+			var t,
+					el = document.createElement("fakeelement");
+
+			var animations = {
+				"animation"      : "animationend",
+				"OAnimation"     : "oAnimationEnd",
+				"MozAnimation"   : "animationend",
+				"WebkitAnimation": "webkitAnimationEnd"
+			}
+
+			for (t in animations){
+				if (el.style[t] !== undefined){
+					return animations[t];
+				}
+			}
 		}
-		setting.ele = $("<div>").addClass("vc-lb-wrapper").attr("id",setting.id).attr("close-btn",setting.close_btn).append(
-			$("<div>").addClass("vc-lb-container").append(
-				$("<div>").addClass("vc-lb-body").html(setting.content)
-			)
-			.bind("mousemove",function(e){
-				var ClientX = e.clientX-this.getBoundingClientRect().left;
-				var ClientY = e.clientY-this.getBoundingClientRect().top;
-				var eleX = this.offsetWidth - 30
-				var eleY = 30;
-				if (ClientX >= eleX && ClientY <= eleY)
-					$(this).addClass("onClose");
-				else
-					$(this).removeClass("onClose");
-			})
-			.bind("touchstart click",function(e){
-				if (setting.close_btn)
-				{
-					var ClientX = e.clientX-this.getBoundingClientRect().left;
-					var ClientY = e.clientY-this.getBoundingClientRect().top;
-					var eleX = this.offsetWidth - 30
-					var eleY = 30;
+		var formYoutube = function(url,w,h){
+			var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+			var match = url.match(regExp);
+			if (match && match[2].length == 11) {
+					return '<div class="vc-lb-youtube"><iframe width="'+(w == null ? '640px' : w) +'" '+
+						'height="'+(h == null ? '360px' : h) +'" '+
+						'src="https://www.youtube.com/embed/'+match[2]+'" frameborder="0" allowfullscreen></iframe></div>'
+			} else {
+					return '<div class="vc-lb-youtube"><iframe width="'+(w == null ? '640px' : w) +'" '+
+						'height="'+(h == null ? '360px' : h) +'" '+
+						'src="'+url+'" frameborder="0" allowfullscreen></iframe></div>';
+			}
+		}
+		var close = function(){
+			$set = setting;
+			$("#"+$set.id).addClass("closing").one(AnimationEvent(), function(event) {
+				$("#"+$set.id).remove();
+				$set.afteClose();
+			});
+		}
 
-					if (ClientX >= eleX && ClientY <= eleY)
-					{
-						$("#"+setting.id).addClass("closing").one(setting.AnimationEvent(), function(event) {
-							$("#"+setting.id).remove();
-						});
-					}
-				}
-			})
-		).append($("<div>").addClass("vc-lb-backdrag")
-			.bind("click",function(e){
-				if (e.target == $(this)[0] && setting.backdrag_close)
-				{
-					$("#"+setting.id).addClass("closing").one(setting.AnimationEvent(), function(event) {
-						$("#"+setting.id).remove();
-					});
-				}
-			})
-		);
-		if (setting.isIE) setting.ele.addClass("isIE");
-		if (setting.width !== null) setting.ele.css({"width":setting.width});
-		if (setting.height !== null) setting.ele.css({"height":setting.height});
+		this.close = function(){
+			close();
+		}
 
-		$("body").append(setting.ele);
+		this.initialize = function(){
+			$set = setting;
+			if ($set.youtubeEmbed && $set.youtubeUrl != "")
+			{
+				$set.content += formYoutube($set.youtubeUrl,$set.width,$set.height);
+			}
+			$set.ele = $("<div>").addClass("vc-lb-wrapper").attr("id",$set.id).attr("close-btn",$set.close_btn).append(
+			  $("<div>").addClass("vc-lb-container").append(
+			    $("<div>").addClass("vc-lb-body").html($set.content)
+			  )
+			  .bind("mousemove",function(e){
+			    var ClientX = e.clientX-this.getBoundingClientRect().left;
+			    var ClientY = e.clientY-this.getBoundingClientRect().top;
+			    var eleX = this.offsetWidth - 30
+			    var eleY = 30;
+			    if (ClientX >= eleX && ClientY <= eleY)
+			      $(this).addClass("onClose");
+			    else
+			      $(this).removeClass("onClose");
+			  })
+			  .bind("touchstart click",function(e){
+			    if ($set.close_btn)
+			    {
+			      var ClientX = e.clientX-this.getBoundingClientRect().left;
+			      var ClientY = e.clientY-this.getBoundingClientRect().top;
+			      var eleX = this.offsetWidth - 30
+			      var eleY = 30;
+
+			      if (ClientX >= eleX && ClientY <= eleY)
+			      {
+							close();
+			      }
+			    }
+			  })
+			).append($("<div>").addClass("vc-lb-backdrag")
+			  .bind("click",function(e){
+			    if (e.target == $(this)[0] && $set.backdrag_close)
+			    {
+			      close();
+			    }
+			  })
+			);
+			if ($set.isIE) $set.ele.addClass("isIE");
+			if ($set.width !== null) $set.ele.css({"width":$set.width});
+			if ($set.height !== null) $set.ele.css({"height":$set.height});
+
+			$("body").append($set.ele);
+			return this;
+		}
+
+		return this.initialize();
 	}
 })(jQuery);
